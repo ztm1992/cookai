ecbdc0b3b67ba049b167bb786c021f8d67adadc6
TAIMIN ZHANG <taiminzhang@TAIMINdeiMac.local>
2023-08-22 23:20:32 +0900

    first cook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cookai",
+    version="0.1",
+    packages=find_packages(),
+    author="Taimin Zhang",
+    description="A delicious AI recipe.",
+    url="https://github.com"
+)
